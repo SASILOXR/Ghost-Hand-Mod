2673f258df71f6527cef3bd4b9fc8d01fd337431
sasiloxr <sasiloxr2@gmail.com>
2025-01-22 16:10:40 +0800

    add config save and load

--- src/main/java/com/sasiloxr/ghosthand/GhostHandMod.java
+++ src/main/java/com/sasiloxr/ghosthand/GhostHandMod.java
@@ -1,13 +1,18 @@
 package com.sasiloxr.ghosthand;
 
+import com.sasiloxr.ghosthand.command.CommandGhostHand;
 import com.sasiloxr.ghosthand.mouseoverhandler.MouseOverHandler;
+import net.minecraftforge.client.ClientCommandHandler;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.common.config.Property;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
+import java.io.File;
 import java.nio.file.AtomicMoveNotSupportedException;
 
 @Mod(modid = GhostHandMod.MODID, name = GhostHandMod.NAME, version = GhostHandMod.VERSION, acceptedMinecraftVersions = "1.8.9")
@@ -16,17 +21,24 @@ public class GhostHandMod
     public static final String MODID = "ghosthand";
     public static final String NAME = "GhostHand";
     public static final String VERSION = "1.0";
+    public static File configFile;
     public static boolean enabled  = true;
+    public static Configuration config;
 
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event){
+        configFile = event.getSuggestedConfigurationFile();
 
     }
-    
+
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
+
+        ClientCommandHandler.instance.registerCommand(new CommandGhostHand());
+        config = new Configuration(configFile);
+        loadConfig();
         MouseOverHandler handler = new MouseOverHandler();
 
     }
@@ -34,5 +46,23 @@ public class GhostHandMod
     @EventHandler
     public void postInit(FMLPostInitializationEvent event){
 
+    }
+    public static void saveConfig(){
+        updateConfig(false);
+        config.save();
+    }
+
+    public static void loadConfig(){
+        config.load();
+        updateConfig(true);
+    }
+
+    public static void updateConfig(boolean load){
+        Property property = config.get("GhostHand", "enable", false);
+        if (load){
+            enabled = property.getBoolean();
+        }else{
+            property.setValue(enabled);
+        }
     }
 }
---
+++ src/main/java/com/sasiloxr/ghosthand/command/CommandGhostHand.java
@@ -0,0 +1,51 @@
+package com.sasiloxr.ghosthand.command;
+
+import com.sasiloxr.ghosthand.GhostHandMod;
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.REUtil;
+import net.minecraft.block.BlockPressurePlate;
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.util.ChatComponentText;
+import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
+
+public class CommandGhostHand extends CommandBase {
+    @Override
+    public String getCommandName() {
+        return "ghosthand";
+    }
+
+    @Override
+    public String getCommandUsage(ICommandSender sender) {
+        return "/ghosthand";
+    }
+
+    @Override
+    public void processCommand(ICommandSender sender, String[] args) throws CommandException {
+        if (args.length > 2){
+            sender.addChatMessage(new ChatComponentText("Invalid Option"));
+            return;
+        }
+        if (args.length == 0){
+            sender.addChatMessage(new ChatComponentText("Usage: /ghosthand [true/false]"));
+        }
+
+        String opt = args[0];
+
+        boolean enabled = Boolean.parseBoolean(opt);
+        if (enabled){
+            GhostHandMod.enabled = true;
+            sender.addChatMessage(new ChatComponentText("GhostHand enable"));
+        }else{
+            GhostHandMod.enabled = false;
+            sender.addChatMessage(new ChatComponentText("GhostHand disable"));
+        }
+        GhostHandMod.saveConfig();
+
+    }
+
+    @Override
+    public boolean canCommandSenderUseCommand(ICommandSender sender) {
+        return true;
+    }
+}
