37364e235cb7fe3451be7664a87d995d83bd8e12
sasiloxr <sasiloxr2@gmail.com>
2025-02-16 20:36:48 +0800

    fix the entitylivefirebox get null bug

--- src/main/java/com/sasiloxr/ghosthand/utils/Utils.java
+++ src/main/java/com/sasiloxr/ghosthand/utils/Utils.java
@@ -11,36 +11,41 @@ import scala.tools.nsc.doc.base.comment.Bold;
 import java.awt.*;
 
 public class Utils {
-   public static Minecraft mc = Minecraft.getMinecraft();
-    public static boolean isTeamMate(Entity teamMate){
-        return mc.thePlayer.isOnSameTeam((EntityLivingBase) teamMate) || isSameColorWithPlayer(teamMate);
+    public static Minecraft mc = Minecraft.getMinecraft();
+
+    public static boolean isTeamMate(Entity teamMate) {
+        if (teamMate instanceof EntityPlayer) {
+            return mc.thePlayer.isOnSameTeam((EntityLivingBase) teamMate) || isSameColorWithPlayer(teamMate);
+        }
+        return false;
     }
-    public static boolean isSameColorWithPlayer(Entity entity){
-        if (!(entity instanceof EntityPlayer)){
+
+    public static boolean isSameColorWithPlayer(Entity entity) {
+        if (!(entity instanceof EntityPlayer)) {
             return false;
         }
         String playerName = mc.thePlayer.getDisplayName().getUnformattedText();
         int index = playerName.indexOf(mc.thePlayer.getDisplayNameString());
-        if (index < 1){
+        if (index < 1) {
             return false;
         }
         String playerColorstring = playerName.substring(0, index);
         int index2 = playerColorstring.lastIndexOf("§");
-        if (index2 < 0){
+        if (index2 < 0) {
             return false;
         }
-        String playerColor = playerColorstring.substring(index2, index2+2);
+        String playerColor = playerColorstring.substring(index2, index2 + 2);
         String entityName = entity.getDisplayName().getUnformattedText();
         int index1 = entityName.indexOf(((EntityPlayer) entity).getDisplayNameString());
-        if (index1 < 1){
+        if (index1 < 1) {
             return false;
         }
         String entityColorString = entityName.substring(0, index1);
         int index3 = entityColorString.lastIndexOf("§");
-        if (index3 < 0){
+        if (index3 < 0) {
             return false;
         }
-        String entityColor = entityColorString.substring(index3, index3+2);
+        String entityColor = entityColorString.substring(index3, index3 + 2);
         return playerColor.equals(entityColor);
     }
 }
